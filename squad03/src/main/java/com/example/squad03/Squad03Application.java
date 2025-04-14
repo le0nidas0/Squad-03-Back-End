e4b9bfd86c022a625bd75c3506605632e8208f54
leonidas <leonidasmoreira098@gmail.com>
2025-04-14 17:22:25 -0300

    Iniciando o projeto Spring com as dependências necessárias.

---
+++ squad03/src/main/java/com/example/squad03/Squad03Application.java
@@ -0,0 +1,13 @@
+package com.example.squad03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Squad03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Squad03Application.class, args);
+	}
+
+}
---
+++ squad03/src/test/java/com/example/squad03/Squad03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squad03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Squad03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
