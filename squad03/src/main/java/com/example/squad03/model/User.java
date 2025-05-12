26a6ad20783bedd5cde233b9676c663865b056a2
Pedro Lucas <pedrolucashneves@outlook.com>
2025-05-11 22:43:56 -0300

    Codigo para Login e cadastro

---
+++ squad03/src/main/java/com/example/squad03/model/User.java
@@ -0,0 +1,22 @@
+package com.example.squad03.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+}
