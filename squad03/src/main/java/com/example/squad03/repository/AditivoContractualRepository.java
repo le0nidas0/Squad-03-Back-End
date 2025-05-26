a6e355275e3d99553f3866bfb55e49e1285efec3
Isaass <isaiasoliver.dev@gmail.com>
2025-05-26 15:34:28 -0300

    feat: adicao do crud de aditivos

---
+++ squad03/src/main/java/com/example/squad03/repository/AditivoContractualRepository.java
@@ -0,0 +1,7 @@
+package com.example.squad03.repository;
+
+import com.example.squad03.model.AditivoContractual;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AditivoContractualRepository extends JpaRepository<AditivoContractual, Long> {
+}
