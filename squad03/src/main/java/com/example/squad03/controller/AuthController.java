7e9bbbce5793bf3558be1e7e9a56ab6c9b2f709b
Pedro Lucas <pedrolucashneves@outlook.com>
2025-05-30 19:38:01 -0300

    Adicionado Authcintroller

---
+++ squad03/src/main/java/com/example/squad03/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.squad03.controller;
+
+import com.example.squad03.dto.AuthRequest;
+import com.example.squad03.dto.AuthResponse;
+import com.example.squad03.dto.RegisterRequest;
+import com.example.squad03.service.impl.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
